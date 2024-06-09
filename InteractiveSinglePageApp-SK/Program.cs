ccaff0ff8a895b915cbd61bff196c47ab6c49081
Alii-s <119129254+Alii-s@users.noreply.github.com>
2024-06-10 00:07:53 +0300

    Logout/Login/Register Functionalities

--- InteractiveSinglePageApp-SK/Program.cs
+++ InteractiveSinglePageApp-SK/Program.cs
@@ -9,12 +9,17 @@ using Microsoft.AspNetCore.Http;
 using System.Text;
 using Microsoft.AspNetCore.Builder;
 using System.Xml.Linq;
+using System.ServiceModel.Syndication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAntiforgery();
+builder.Services.AddDistributedMemoryCache();
 var connectionString = "Data Source=wwwroot/database.db";
 builder.Services.AddSingleton<IDbConnection>(_ => new SqliteConnection(connectionString));
-
 using (var connection = new SqliteConnection(connectionString))
 {
     connection.Open();
@@ -36,21 +41,203 @@ using (var connection = new SqliteConnection(connectionString))
                     )
                     """);
 }
-
-
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 app.MapFallbackToFile("/index.html");
 app.UseAntiforgery();
 app.UseStaticFiles();
 app.UseHttpsRedirection();
+app.UseSession();
+
+app.MapPost("/register", async (HttpContext context, [FromForm] string email, [FromForm] string password, IDbConnection db, IAntiforgery antiforgery) =>
+{
+    await antiforgery.ValidateRequestAsync(context);
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        var user = connection.QuerySingleOrDefault<User>("SELECT * FROM Users WHERE email = @Email", new { Email = email });
+        if (user != null)
+        {
+            string htmlContent = $"""<div class="alert alert-danger" role="alert">User Already Exists.</div>""";
+            return Results.Content(htmlContent, "text/html");
+        }
+        string passwordHash = BCrypt.Net.BCrypt.HashPassword(password);
+        connection.Execute("INSERT INTO Users (email, password) VALUES (@Email, @PasswordHash)", new { Email = email, PasswordHash = passwordHash });
+        return Results.Ok();
+    }
+});
+
+app.MapGet("/api/tokens", (HttpContext context, IAntiforgery antiforgery) =>
+{
+    var token = antiforgery.GetAndStoreTokens(context);
+    string html = $"""<input name = "{token.FormFieldName}" type = "hidden" value = "{token.RequestToken}"/>""";
+    return Results.Content(html, "text/html");
+});
+
+app.MapPost("/login", async (HttpContext context, [FromForm] string email, [FromForm] string password, IDbConnection db,IAntiforgery antiforgery) =>
+{
+    await antiforgery.ValidateRequestAsync(context);
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        var user = connection.QuerySingleOrDefault<User>("SELECT * FROM Users WHERE email = @Email", new { Email = email });
+        if (user == null || !BCrypt.Net.BCrypt.Verify(password, user.password))
+        {
+            string htmlContent = $"""<div class="alert alert-danger loginMessage" role="alert">Invalid email or password</div>""";
+            return Results.Content(htmlContent,"text/html");
+        }
+        context.Session.SetInt32("UserId", (int)user.Id);
+        string success = $"""<div class="alert alert-success loginMessage" role="alert">Login Successful</div>""";
+        return Results.Content(success, "text/html");
+    }
+});
+
+app.MapGet("/logout", async (HttpContext context) =>
+{
+    context.Session.Remove("UserId");
+    return Results.NoContent();
+});
+
+app.MapGet("/home", async (HttpContext context, IDbConnection db) =>
+{
+    var userId = context.Session.GetInt32("UserId");
+    if (userId == null)
+    {
+        return Results.Redirect("/login");
+    }
+
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        var feeds = connection.Query<Feed>("SELECT * FROM Feeds WHERE UserId = @UserId", new { UserId = userId });
+        return Results.Content("<div>Testing</div>","text/html");
+    }
+});
+
+app.MapPost("/addFeed", async (HttpContext context, [FromForm] string url, IDbConnection db) =>
+{
+    var userId = context.Session.GetInt32("UserId");
+    if (userId == null)
+    {
+        return Results.Redirect("/login");
+    }
+
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        connection.Execute("INSERT INTO Feeds (UserId, Url) VALUES (@UserId, @Url)", new { UserId = userId, Url = url });
+    }
+    return Results.Redirect("/home");
+});
+
+app.MapDelete("/removeFeed", async (HttpContext context, [FromForm] int id, IDbConnection db) =>
+{
+    var userId = context.Session.GetInt32("UserId");
+    if (userId == null)
+    {
+        return Results.Redirect("/login");
+    }
+
+    using (var connection = new SqliteConnection(connectionString))
+    {
+        connection.Execute("DELETE FROM Feeds WHERE Id = @Id AND UserId = @UserId", new { Id = id, UserId = userId });
+    }
+    return Results.Redirect("/home");
+});
+
+
+app.MapGet("/loginPage", async (HttpContext context, IAntiforgery antiforgery) =>
+{
+    var token = antiforgery.GetAndStoreTokens(context);
+    string htmlContent = $"""
+        <div class="d-flex justify-content-center" id="pageTopRow">
+        <div class="card" id="quickSignIn">
+            <div class="card-body">
+                <h5 class="card-title h3 pt-3">Hello!</h5>
+                <p class="card-text">Please Log In</p>
+                <form class="login" hx-post="/login" hx-target=".loginError">
+                    <input name = "{token.FormFieldName}" type = "hidden" value = "{token.RequestToken}"/>
+                    <div class="input-control">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="text" class="form-control mb-2" id="email" placeholder="name@example.com" name="email">
+                        <div class="error form-text mb-1"></div>
+                    </div>
+                    <div class="input-control password">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" id="password" class="form-control mb-2" placeholder="password" name="password">
+                        <div class="error form-text"></div>
+                    </div>
+                    <button disabled class="btn btn-outline-dark mt-2" type="submit" id="login">Log In</button>
+                </form>
+                <a hx-get="/registerPage" hx-target=".replace" class="form-text register">Don't have an account? Register
+                    Now!</a>
+                <div class="loginError"><div>
+            </div>
+        </div>
+    </div>
+    """;
+    return Results.Content(htmlContent, "text/html");
+});
+
+app.MapGet("/registerPage", async (HttpContext context, IAntiforgery antiforgery) =>
+{
+    var token = antiforgery.GetAndStoreTokens(context);
+    string htmlContent = $"""
+    <div class="d-flex justify-content-center" id="pageTopRow">
+        <div class="card" id="quickSignIn">
+            <div class="card-body">
+                <h5 class="card-title h3 pt-3">Create a new account</h5>
+                <p class="card-text">Please fill the required info</p>
+                <form hx-post="/register" hx-target=".registerError" novalidate class="register">
+                    <input name = "{token.FormFieldName}" type = "hidden" value = "{token.RequestToken}"/>
+                    <div class="input-control">
+                        <label for="email" class="form-label">Email address</label>
+                        <input type="text" class="form-control mb-2" id="email" placeholder="name@example.com" name="email">
+                        <div class="error"></div>
+                    </div>
+                    <div class="input-control password">
+                        <label for="inputPassword5" class="form-label">Password</label>
+                        <input type="password" id="password" class="form-control mb-3 password"
+                            aria-describedby="passwordHelpBlock mb-0" placeholder="password" name="password">
+                        <div class="error"></div>
+                    </div>
+                    <div class="input-control password">
+                        <label for="passwordConf" class="form-label">Confirm Password</label>
+                        <input type="password" id="passwordConf" class="form-control mb-3" required
+                            aria-describedby="passwordHelpBlock mb-0" placeholder="password">
+                        <div class="error"></div>
+                    </div>
+                    <button class="btn btn-outline-dark mt-2" type="submit" disabled id="registerBtn">Register</button>
+                    <div class="registerError"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+    """;
+    return Results.Content(htmlContent,"text/html");
+});
 
 app.Run();
-
 public class User
 {
     public int? Id { get; set; }
     public string? email { get; set; }
-
     public string? password { get; set; }
 }
+
+public class Feed
+{
+    public int? Id { get; set; }
+    public int? UserId { get; set; }
+    public string? Url { get; set; }
+}
+
--- InteractiveSinglePageApp-SK/wwwroot/index.js
+++ InteractiveSinglePageApp-SK/wwwroot/index.js
@@ -1,94 +1,154 @@
 $(function () {
-    $(document).scroll(function () {
-      var $nav = $("#mainNavbar");
-      $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-    });
-  })
-  
-  
-  const form = document.querySelector('form.login');
-  const email = document.querySelector('#email');
-  const password = document.querySelector('#password');
-  const login = document.querySelector('#login');
-  const card = document.querySelector('.card');
-  const showPassword = document.querySelector('#showPassword');
-  const faEye = document.querySelector('.fa-eye');
-  const faEyeSlash = document.querySelector('.fa-eye-slash');
-  const support=document.querySelector('#support')
-  let successFlagP = false;
-  let successFlagE = false;
-  // validation
-  const isValidEmail = email => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+  $(document).scroll(function () {
+    var $nav = $("#mainNavbar");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+})
+
+
+let formLogin = document.querySelector('form.login');
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+const login = document.querySelector('#login');
+const card = document.querySelector('.card');
+
+let successFlagP = false;
+let successFlagE = false;
+// validation
+const isValidEmail = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+formLogin.addEventListener('change', function (e) {
+  validateInputs();
+})
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
+}
+
+const setSuccess = (element) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.innerText = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+}
+
+const validateInputs = () => {
+  successFlagP = false;
+  successFlagE = false;
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  if (emailValue === '') {
+    setError(email, 'Email is required.');
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, 'Please enter a valid email.');
+  } else {
+    setSuccess(email);
+    successFlagE = true;
   }
-  
-  form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    e.stopImmediatePropagation();
-    validateInputs();
-  })
-  
-  const setError = (element, message) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-  
-    errorDisplay.innerText = message;
-    inputControl.classList.add('error');
-    inputControl.classList.remove('success');
+
+  if (passwordValue === '') {
+    setError(password, 'Password is required.');
+  } else if (passwordValue.length < 8) {
+    setError(password, 'Password must be at least 8 characters.');
+  } else {
+    setSuccess(password);
+    successFlagP = true;
   }
-  
-  const setSuccess = (element) => {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error');
-  
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
+  if (successFlagP && successFlagE) {
+    document.querySelector("#login").disabled = false;
+    console.log('success')
+  } else {
+    document.querySelector("#login").disabled = true;
   }
-  
-  const validateInputs = () => {
-    successFlagP = false;
-    successFlagE = false;
-    const emailValue = email.value.trim();
-    const passwordValue = password.value.trim();
-    if (emailValue === '') {
-      setError(email, 'Email is required.');
-    } else if (!isValidEmail(emailValue)) {
-      setError(email, 'Please enter a valid email.');
-    } else {
-      setSuccess(email);
-      successFlagE = true;
+  // validation end
+}
+validateInputs();
+
+const validateInputsRegister = () => {
+  successFlagP = false;
+  successFlagE = false;
+  successFlagPC = false;
+  const registerButton = document.querySelector('#registerBtn');
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const passwordConfValue = passwordConf.value.trim();
+  if (emailValue === '') {
+    setError(email, 'Email is required.');
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, 'Please enter a valid email.');
+  } else {
+    setSuccess(email);
+    successFlagE = true;
+  }
+  if (passwordValue === '') {
+    setError(password, 'Password is required.');
+  } else if (passwordValue.length < 8 || passwordValue.length > 20) {
+    setError(password, 'Password must be between 8 & 20 characters.');
+  } else {
+    setSuccess(password);
+    successFlagP = true;
+  }
+  if (passwordConfValue !== passwordValue || passwordConfValue === '') {
+    setError(passwordConf, 'Please confirm your password.')
+  } else {
+    setSuccess(passwordConf);
+    successFlagPC = true;
+  }
+  if (successFlagP && successFlagPC && successFlagE) {
+    document.querySelector("#registerBtn").disabled = false;
+  } else {
+    document.querySelector("#registerBtn").disabled = true;
+  }
+}
+document.body.addEventListener('htmx:afterRequest', function (evt) {
+  console.log(evt);
+  email = document.querySelector('#email');
+  password = document.querySelector('#password');
+  if (evt.detail.requestConfig.verb === 'get') {
+    console.log("hi")
+    console.log(evt.detail.requestConfig.path)
+    formLogin = document.querySelector('form.login');
+    if (evt.detail.requestConfig.path === '/loginPage') {
+      console.log('login page loaded')
+      formLogin.addEventListener('change', function (e) {
+        validateInputs();
+      })
+      validateInputs();
+      console.log('login page loaded')
+    } else if (evt.detail.requestConfig.path === '/registerPage') {
+      console.log('register page loaded')
+      let passwordConf = document.querySelector('#passwordConf');
+      let formRegister = document.querySelector('form.register');
+      formRegister.addEventListener('change', function (e) {
+        validateInputsRegister();
+      })
+      validateInputsRegister();
+    } else if (evt.detail.requestConfig.path === '/logout') {
+      console.log('logout post')
+      htmx.ajax('GET', '/loginPage', { target: '.replace' });
+      document.querySelector('.loginButton').classList.toggle('d-none');
+      document.querySelector('.logoutNav').classList.toggle('d-none');
     }
-  
-    if (passwordValue === '') {
-      setError(password, 'Password is required.');
-      faEye.style.bottom = '40%'
-    } else if (passwordValue.length < 8) {
-      setError(password, 'Password must be at least 8 characters.');
-      faEye.style.bottom = '40%'
-    } else {
-      setSuccess(password);
-      successFlagP = true;
-    }
-    if (successFlagP && successFlagE) {
-      window.location.href = "../TEA-BANK.HTML"
-  
-    }
-    // validation end
-  
-  }
-    showPassword.addEventListener('click', function (e) {
-      this.classList.toggle("fa-eye");
-      this.classList.toggle('fa-eye-slash');
-      if (password.getAttribute('type') === 'password') {
-        password.setAttribute('type', 'text');
+  }//
+  if (evt.detail.requestConfig.verb === 'post') {
+    console.log("detected post")
+    if (evt.detail.requestConfig.path === '/login') {
+      console.log('login post')
+      if (document.querySelector('.loginMessage').innerHTML === 'Login Successful') {
+        htmx.ajax('GET', '/home', { target: '.replace' });
+        document.querySelector('.loginButton').classList.toggle('d-none');
+        document.querySelector('.logoutNav').classList.toggle('d-none');
       }
-      else {
-        password.setAttribute('type', 'password');
-      }
-    })
-    if (!(localStorage.getItem('isLoggedIn') === 'true')) {  
-      support.classList.toggle('d-none')
     }
-  
+  }
+});
